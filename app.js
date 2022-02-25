d1a7aa50b1bacc951147959b52845cdf6e430d75
정미니 <alsgml1640@hanmail.net>
2022-02-25 22:35:00 +0900

    캔버스에 그림 그리기 기능 추가 및 색상 변경 기능 추가

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
 const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
 
 const range = document.querySelector("#range");
 
 const color = document.getElementsByClassName("color");
 
-canvas.addEventListener("mousemove", (event) => {
-  console.log(event.offsetX, event.offsetY);
-  //   console.log(canvas.offsetLeft);
-});
+let painting = false;
 
-Array.from(color).forEach((e) => {
-  const selectColor = e.style.backgroundColor;
+ctx.fillStyle = "black";
+ctx.strokeStyle = "black";
+
+ctx.lineWidth = 2.5;
+
+canvas.width = 700;
+canvas.height = 700;
+
+// 채우기 효과
+function handleCanvasClick() {}
+
+Array.from(color).forEach((color) => {
+  color.addEventListener("click", () => {
+    const selectColor = color.style.backgroundColor;
+    ctx.strokeStyle = selectColor;
+  });
 });
 
 range.addEventListener("input", () => {
   const size = range.value;
+  ctx.lineWidth = size;
 });
+
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting);
+canvas.addEventListener("click", handleCanvasClick);
+
+function startPainting() {
+  painting = true;
+}
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
