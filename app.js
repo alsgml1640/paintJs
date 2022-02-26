fe5a212547bf6e5ff15e2d4b90f1baf9fddb8df0
정미니 <alsgml1640@hanmail.net>
2022-02-26 21:44:56 +0900

    JS 그림판 만들기 셀프 구현 업데이트

--- app.js
+++ app.js
@@ -5,36 +5,40 @@ const range = document.querySelector("#range");
 
 const color = document.getElementsByClassName("color");
 
+const fillBtn = document.querySelector(".fillBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const saveBtn = document.querySelector(".saveBtn");
+
+const CANVAS_SIZE = 700;
+
 let painting = false;
+let filling = false;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
 ctx.fillStyle = "black";
 ctx.strokeStyle = "black";
 
 ctx.lineWidth = 2.5;
 
-canvas.width = 700;
-canvas.height = 700;
-
-// 채우기 효과
-function handleCanvasClick() {}
-
 Array.from(color).forEach((color) => {
-  color.addEventListener("click", () => {
-    const selectColor = color.style.backgroundColor;
-    ctx.strokeStyle = selectColor;
-  });
+  color.addEventListener("click", handleColorClick);
 });
 
-range.addEventListener("input", () => {
+function handleColorClick(event) {
+  const selectColor = event.target.style.backgroundColor;
+  ctx.strokeStyle = selectColor;
+  ctx.fillStyle = selectColor;
+}
+
+function handleRangeSize() {
   const size = range.value;
   ctx.lineWidth = size;
-});
-
-canvas.addEventListener("mousemove", onMouseMove);
-canvas.addEventListener("mousedown", startPainting);
-canvas.addEventListener("mouseup", stopPainting);
-canvas.addEventListener("mouseleave", stopPainting);
-canvas.addEventListener("click", handleCanvasClick);
+}
 
 function startPainting() {
   painting = true;
@@ -56,3 +60,44 @@ function onMouseMove(event) {
     ctx.stroke();
   }
 }
+
+// 채우기 효과
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleFillBtnClick() {
+  const btnName = fillBtn.innerHTML;
+  if (btnName === "FILL") {
+    fillBtn.innerHTML = "PAINT";
+    filling = true;
+  } else {
+    fillBtn.innerHTML = "FILL";
+    filling = false;
+  }
+}
+
+function handleClearBtnClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+function handleSaveClick() {
+  const link = document.createElement("a");
+  link.download = "paintImg";
+  link.href = canvas.toDataURL();
+  link.click();
+}
+
+clearBtn.addEventListener("click", handleClearBtnClick);
+fillBtn.addEventListener("click", handleFillBtnClick);
+saveBtn.addEventListener("click", handleSaveClick);
+
+range.addEventListener("input", handleRangeSize);
+
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mouseleave", stopPainting);
+canvas.addEventListener("click", handleCanvasClick);
